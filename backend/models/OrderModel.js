8c02635d3a163316454629b18d8df2d5932611de
Himanshu Soni <himanshuso@iitbhilai.ac.in>
2025-08-02 16:04:01 +0530

    Removed nested Git repository from tracking

--- backend/models/OrderModel.js
+++
@@ -1,6 +0,0 @@
-const { model } = require("mongoose");
-const { OrdersSchema } = require("../schemas/OrdersSchema");
-
-const OrderModel = model("Order", OrdersSchema); // ✅ Capitalize model name (optional but standard)
-
-module.exports = { OrderModel };
